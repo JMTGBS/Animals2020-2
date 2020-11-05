25dcba89c91b124b3efca1f91b45c0dcab025c68
ChaerinMin <43366923+ChaerinMin@users.noreply.github.com>
2020-11-05 23:02:23 +0900

    카메라 구현

--- Final/FinalProject/app/src/main/java/org/techtown/finalproject/Creating.java
+++ Final/FinalProject/app/src/main/java/org/techtown/finalproject/Creating.java
@@ -1,18 +1,32 @@
 package org.techtown.finalproject;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Creating#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
+
 public class Creating extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -24,41 +38,68 @@ public class Creating extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Creating() {
-        // Required empty public constructor
-    }
+    ImageView openCamera;
+    File imageFile;
+    Uri uri;
+    Context context;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Creating.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Creating newInstance(String param1, String param2) {
-        Creating fragment = new Creating();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context=context;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onDetach() {
+        super.onDetach();
+        context=null;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_creating, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_creating, container, false);
+        openCamera = rootView.findViewById(R.id.picture);
+        imageFile = new File(Environment.getExternalStorageDirectory(),"pictureTaken.jpg");
+        openCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    imageFile.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if(Build.VERSION.SDK_INT >= 24) {
+                    uri = FileProvider.getUriForFile(context, "org.techtown.finalproject.intent.fileprovider", imageFile);
+                }else{
+                    uri = Uri.fromFile(imageFile);
+                }
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION|Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                startActivityForResult(intent, 101);
+            }
+        });
+        return rootView;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 101 && resultCode == RESULT_OK) {
+            Bitmap bitmap = null;
+            try {
+                bitmap = BitmapFactory.decodeStream(context.getContentResolver().openInputStream(uri));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            //Log.d("mylog","여기까지 왔다");
+            openCamera.setImageBitmap(bitmap);
+        }
+    }
+
+
 }
--- Final/FinalProject/app/src/main/java/org/techtown/finalproject/MainActivity.java
+++ Final/FinalProject/app/src/main/java/org/techtown/finalproject/MainActivity.java
@@ -3,12 +3,66 @@ package org.techtown.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.pedro.library.AutoPermissions;
+import com.pedro.library.AutoPermissionsListener;
+
+public class MainActivity extends AppCompatActivity implements AutoPermissionsListener {
+
+    Creating creating;
+    Analyzing analyzing;
+    Viewing viewing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        creating = new Creating();
+        analyzing = new Analyzing();
+        viewing = new Viewing();
+
+        Button button1 = findViewById(R.id.creating);
+        Button button2 = findViewById(R.id.viewing);
+        Button button3 = findViewById(R.id.analyzing);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.overview,creating).commit();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.overview,viewing).commit();
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.overview,analyzing).commit();
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        AutoPermissions.Companion.parsePermissions(this, requestCode, permissions, this);
+    }
+
+    @Override
+    public void onDenied(int requestCode, String[] permissions) {
+        Toast.makeText(this, "permissions denied : " + permissions.length, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onGranted(int requestCode, String[] permissions) {
+        Toast.makeText(this, "permissions granted : " + permissions.length, Toast.LENGTH_LONG).show();
     }
 }
